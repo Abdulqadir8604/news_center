f3151047c5b38e8940f654554d36074a477968b4
Abdulqadir <abdulqadir.b8604@gmail.com>
2022-08-06 10:48:13 +0530

    update

--- app/src/main/java/com/lamaq/newscenter/MainActivity.kt
+++ app/src/main/java/com/lamaq/newscenter/MainActivity.kt
@@ -2,17 +2,27 @@ package com.lamaq.newscenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
         val recyclerView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this, androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL, false)
         val data = ArrayList<Model>()
+
+        val articles = NewsApi.RetrofitHelper.getInstance().create(NewsList::class.java).articles
+        GlobalScope.launch {
+            val result = NewsApi.RetrofitHelper.getInstance().create(NewsList::class.java).articles
+            if (result != null)
+            // Checking the results
+                result.forEach {
+                    data.add(Model(it.title, it.description, it.urlToImage))
+                }
+        }
+
 
         for (i in 0..10) {
             data.add(Model("Title $i", "Description $i", "https://picsum.photos/200/300?random=$i"))
--- app/src/main/java/com/lamaq/newscenter/NewsApi.kt
+++ app/src/main/java/com/lamaq/newscenter/NewsApi.kt
@@ -1,6 +1,8 @@
 package com.lamaq.newscenter
 
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,5 +13,17 @@ interface NewsApi {
 
     @GET("/v2/top-headlines?apiKey=$API_KEY")
     fun getNews(@Query("country") country: String, @Query("page") page:Int): Call<Articles>
+
+
+    object RetrofitHelper {
+
+        fun getInstance(): Retrofit {
+            return Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                // we need to add converter factory to
+                // convert JSON object to Java object
+                .build()
+        }
+    }
 
 }
